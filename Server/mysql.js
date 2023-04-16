83b16a08db8b73254ee2d8e35717fa5906d944c0
Aman Prajapat <113405958+amanprajapat08278@users.noreply.github.com>
2023-04-16 16:17:48 +0530

    Update mysql.js

--- Server/mysql.js
+++ Server/mysql.js
@@ -3,7 +3,13 @@ const dotenv = require("dotenv")
 
 dotenv.config()
 
-const sqlModel = mysql.createConnection(process.env.DATABASE_URL)
+const sqlModel = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    port: process.env.port,
+    database : process.env.database
+})
 
 sqlModel.connect((err) => {
     if (err) { console.log(err.message) }
